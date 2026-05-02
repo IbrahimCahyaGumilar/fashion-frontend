f1d6b700b04f46deb7711dc62c543de43855802a
Ibrahim Cahya Gumilar <ibrahimcahya08@gmail.com>
2026-05-02 17:20:11 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { getMe } from "./features/authSlice";
 
 // Import Pages
@@ -23,75 +23,54 @@ import PrivateRoute from './routes/PrivateRoute';
 import Layout from './components/Layout';
 import LoadingScreen from './components/common/LoadingScreen';
 
-// 1. Komponen ini menangani Logika Loading setiap pindah halaman dan sinkronisasi auth
+// 1. Komponen ini menangani Logika Loading setiap pindah halaman
 const AppContent = () => {
   const dispatch = useDispatch();
-  
-  // Ambil isLoading dari Redux Auth (digunakan untuk getMe)
-  const { isLoading: authLoading } = useSelector((state) => state.auth);
 
-  // State untuk loading animasi (visual)
-  const [manualLoading, setManualLoading] = useState(true);
-  const location = useLocation();
-
-  // Jalankan pengecekan user saat aplikasi pertama kali dimuat
   useEffect(() => {
     dispatch(getMe());
   }, [dispatch]);
 
-  // Efek loading visual setiap perpindahan rute
+
+  const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
+
   useEffect(() => {
-    setManualLoading(true);
+    setIsLoading(true);
     const timer = setTimeout(() => {
-      setManualLoading(false);
-    }, 1200); // Durasi animasi loading
+      setIsLoading(false);
+    }, 1800);
     return () => clearTimeout(timer);
   }, [location.pathname]);
 
-  /**
-   * KUNCI PERBAIKAN:
-   * Aplikasi dianggap loading total jika:
-   * 1. Redux sedang mengambil data user (authLoading)
-   * 2. Animasi perpindahan halaman sedang berjalan (manualLoading)
-   */
-  const isAppStillLoading = authLoading || manualLoading;
-
   return (
     <>
-      {/* Tampilkan Loading Screen jika salah satu proses loading masih berjalan */}
       <AnimatePresence mode="wait">
-        {isAppStillLoading && <LoadingScreen key="app-loading-screen" />}
+        {isLoading && <LoadingScreen key={location.pathname} />}
       </AnimatePresence>
 
-      {/* 
-          HANYA render Routes jika loading sudah selesai. 
-          Ini mencegah PrivateRoute melakukan redirect ke /login 
-          saat data user dari getMe belum sampai.
-      */}
-      {!isAppStillLoading && (
-        <Routes location={location}>
-          {/* Public Routes */}
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/tes" element={<Tes />} />
-          <Route path="/blog/:slug" element={<BlogDetail />} />
+      <Routes location={location} key={location.pathname}>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/tes" element={<Tes />} />
+        <Route path="/blog/:slug" element={<BlogDetail />} />
 
-          {/* Protected Routes (User & Admin) */}
-          <Route element={<PrivateRoute allowedRoles={["admin", "user"]} />}>
-            <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
-            <Route path="/manage-blog" element={<Layout><ManageBlog /></Layout>} />
-          </Route>
+        {/* Protected Routes */}
+        <Route element={<PrivateRoute allowedRoles={["admin", "user"]} />}>
+          <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+          <Route path="/manage-blog" element={<Layout><ManageBlog /></Layout>} />
+        </Route>
 
-          {/* Admin Routes Only */}
-          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
-            <Route path="/manage-user" element={<Layout><ManageUsers /></Layout>} />
-          </Route>
-        </Routes>
-      )}
+        {/* Admin Routes */}
+        <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
+          <Route path="/manage-user" element={<Layout><ManageUsers /></Layout>} />
+        </Route>
+      </Routes>
     </>
   );
 };
